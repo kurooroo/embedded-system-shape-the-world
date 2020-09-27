c864ad6281239e6b47dd46f50d03d6d00b1831de
nta3100 <githubthing+nta3100@users.noreply.github.com>
2020-09-28 00:36:39 +0700

    update lab 16

--- Lab16/Lab16.ino
+++ Lab16/Lab16.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "TUAN-ANH";
-const char* pwd = "0352901241";
+const char* ssid = "ssid";
+const char* pwd = "password";
 
 void setup() {
   // put your setup code here, to run once:
